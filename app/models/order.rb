96629e3f3cd3e07fb7d7db07be283c5ac26c823b
Chang-Woo Rhee <x1wins@changwoo.net>
2021-10-24 01:05:51 -0700

    Added validate in order, customs

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :shipping
+  validates :weight, numericality: { only_float: true }
+  validates :order_number, presence: true
+  validates :product_name, presence: true
+  validates :count, numericality: { only_integer: true }
+  validates :price, numericality: { only_integer: true }
+  validates :hs_code, numericality: { only_integer: true }
 end
