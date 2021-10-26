390fa0862383c4a2d85002d7f5124bf0902c9c7d
Chang-Woo Rhee <x1wins@changwoo.net>
2021-10-26 01:35:52 -0700

    Changed fake value range

--- test/factories/address.rb
+++ test/factories/address.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :address do
-    owner_name { Faker::Name.last_name }
+    owner_name { "#{Faker::Name.first_name} #{Faker::Name.last_name}" }
     ssn { Faker::IDNumber.brazilian_citizen_number(formatted: true) }
     phone1 { Faker::PhoneNumber.phone_number_with_country_code }
     phone2 { Faker::PhoneNumber.phone_number_with_country_code }
-    address1 { "#{Faker::Address.street_name} #{Faker::Address.city}" }
-    address2 { "#{Faker::Address.secondary_address} #{Faker::Address.street_address}" }
+    address1 { "#{Faker::Address.secondary_address} #{Faker::Address.street_address}" }
+    address2 { "#{Faker::Address.street_name} #{Faker::Address.city} #{Faker::Address.state} #{Faker::Address.country}" }
     zipcode { Faker::Address.zip_code }
   end
   factory :to_address, class: ToAddress, parent: :address do
--- test/factories/orders.rb
+++ test/factories/orders.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :order, class: Order do
-    weight { Faker::Number.between(from: 0.00, to: 30.00) }
+    weight { Faker::Number.decimal_part(digits: 2).to_f + Faker::Number.decimal_part(digits: 2).to_f / 100 }
     order_number { Faker::IDNumber.valid }
     product_name { Faker::Game.title }
     count { Faker::Number.within(range: 1..10) }
-    price { Faker::Number.within(range: 100..10000) }
+    price { Faker::Number.within(range: 1..300) }
     hs_code { Faker::Number.within(range: 1..3) }
   end
 end
