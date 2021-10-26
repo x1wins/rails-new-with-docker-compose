cb599b64a1e2185eaee5f148533c77a15b591237
Chang-Woo Rhee <x1wins@changwoo.net>
2021-10-26 14:23:12 -0700

    Fixed search in to_address WORKING GOOD

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -10,6 +10,12 @@ class Shipping < ApplicationRecord
   accepts_nested_attributes_for :to_address, allow_destroy: true, update_only: true
   accepts_nested_attributes_for :from_address, allow_destroy: true, update_only: true
   scope :with_name_like, lambda { |search|
-    joins(:to_address).where("addresses.owner_name LIKE :search", search: "%#{search}%") if search.present?
+    joins(:to_address).where("addresses.owner_name LIKE :search
+OR addresses.ssn LIKE :search
+OR addresses.phone1 LIKE :search
+OR addresses.phone2 LIKE :search
+OR addresses.address1 LIKE :search
+OR addresses.address2 LIKE :search
+OR addresses.zipcode LIKE :search", search: "%#{search}%") if search.present?
   }
 end
