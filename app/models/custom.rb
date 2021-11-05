429657d32afd0645b58796383a9382fc9a86c806
Chang-Woo Rhee <x1wins@changwoo.net>
2021-11-04 18:38:51 -0700

    Added enum

--- app/models/custom.rb
+++ app/models/custom.rb
@@ -1,4 +1,6 @@
 class Custom < ApplicationRecord
+  enum customs_types: { 목록통관: 1, 일반통관: 2 }
+  enum recipient_types: { 개인: 1, 법인: 2 }
   belongs_to :shipping
   validates :customs_type, numericality: { only_integer: true }
   validates :recipient_type, numericality: { only_integer: true }
