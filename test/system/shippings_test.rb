d8e293241ea097ddcc34f39e839c2ecfc706fa58
Chang-Woo Rhee <x1wins@changwoo.net>
2021-11-16 18:00:10 -0800

    Added input form for testing with fill_in

--- test/system/shippings_test.rb
+++ test/system/shippings_test.rb
@@ -24,9 +24,8 @@ class ShippingsTest < ApplicationSystemTestCase
   test "creating a Shipping" do
     visit shippings_url
     click_on "New Shipping"
-
+    input_form
     click_on "Create Shipping"
-
     assert_text "Shipping was successfully created"
     click_on "Back"
   end
@@ -34,7 +33,7 @@ class ShippingsTest < ApplicationSystemTestCase
   test "updating a Shipping" do
     visit shippings_url
     click_on "Edit", match: :first
-
+    input_form
     click_on "Update Shipping"
 
     assert_text "Shipping was successfully updated"
@@ -48,5 +47,43 @@ class ShippingsTest < ApplicationSystemTestCase
     end
 
     assert_text "Shipping was successfully destroyed"
+  end
+
+  def input_form
+    # Customs
+    select Custom.customs_types.key(@custom_attributes[:customs_type]).to_s, from: "Customs type"
+    select Custom.recipient_types.key(@custom_attributes[:recipient_type]).to_s, from: "Recipient type"
+    sleep 1
+    # Order
+    fill_in 'Weight', with: @order_attributes[:weight]
+    fill_in 'Order number', with: @order_attributes[:order_number]
+    fill_in 'Product name', with: @order_attributes[:product_name]
+    fill_in 'Count', with: @order_attributes[:count]
+    fill_in 'Price', with: @order_attributes[:price]
+    fill_in 'Hs code', with: @order_attributes[:hs_code]
+    sleep 1
+    # Parcel
+    fill_in 'Box count', with: @parcel_attributes[:box_count]
+    fill_in 'Memo', with: @parcel_attributes[:memo]
+    fill_in 'Status', with: @parcel_attributes[:status]
+    sleep 1
+    # To Address
+    fill_in 'shipping_to_address_attributes_owner_name', with: @to_address_attributes[:owner_name]
+    fill_in 'shipping_to_address_attributes_ssn', with: @to_address_attributes[:ssn]
+    fill_in 'shipping_to_address_attributes_phone1', with: @to_address_attributes[:phone1]
+    fill_in 'shipping_to_address_attributes_phone2', with: @to_address_attributes[:phone2]
+    fill_in 'shipping_to_address_attributes_address1', with: @to_address_attributes[:address1]
+    fill_in 'shipping_to_address_attributes_address2', with: @to_address_attributes[:address2]
+    fill_in 'shipping_to_address_attributes_zipcode', with: @to_address_attributes[:zipcode]
+    sleep 1
+    # From Address
+    fill_in 'shipping_from_address_attributes_owner_name', with: @from_address_attributes[:owner_name]
+    fill_in 'shipping_from_address_attributes_ssn', with: @from_address_attributes[:ssn]
+    fill_in 'shipping_from_address_attributes_phone1', with: @from_address_attributes[:phone1]
+    fill_in 'shipping_from_address_attributes_phone2', with: @from_address_attributes[:phone2]
+    fill_in 'shipping_from_address_attributes_address1', with: @from_address_attributes[:address1]
+    fill_in 'shipping_from_address_attributes_address2', with: @from_address_attributes[:address2]
+    fill_in 'shipping_from_address_attributes_zipcode', with: @from_address_attributes[:zipcode]
+    sleep 1
   end
 end
