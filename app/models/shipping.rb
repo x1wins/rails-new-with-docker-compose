34351bdc22fdc399f8a07a4fc8d1b2c58e22a54d
Chang-Woo Rhee <x1wins@changwoo.net>
2022-01-07 10:45:11 -0800

    Added address all column to pg_search_documents table

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,19 +1,57 @@
 class Shipping < ApplicationRecord
   include PgSearch::Model
-  multisearchable against: [:order_product_name, :parcel_memo, :to_address_owner_name, :from_address_owner_name]
+  multisearchable against: [:order_product_name, :parcel_memo,
+      :to_address_ssn, :to_address_phone1, :to_address_phone2, :to_address_address1, :to_address_address2, :to_address_zipcode,
+      :from_address_ssn, :from_address_phone1, :from_address_phone2, :from_address_address1, :from_address_address2, :from_address_zipcode]
   def order_product_name
     order.product_name
   end
   def parcel_memo
     parcel.memo
   end
+
   def to_address_owner_name
     to_address.owner_name
+  end
+  def to_address_ssn
+    to_address.ssn
+  end
+  def to_address_phone1
+    to_address.phone1
+  end
+  def to_address_phone2
+    to_address.phone2
+  end
+  def to_address_address1
+    to_address.address1
+  end
+  def to_address_address2
+    to_address.address2
+  end
+  def to_address_zipcode
+    to_address.zipcode
   end
   def from_address_owner_name
     from_address.owner_name
   end
-
+  def from_address_ssn
+    from_address.ssn
+  end
+  def from_address_phone1
+    from_address.phone1
+  end
+  def from_address_phone2
+    from_address.phone2
+  end
+  def from_address_address1
+    from_address.address1
+  end
+  def from_address_address2
+    from_address.address2
+  end
+  def from_address_zipcode
+    from_address.zipcode
+  end
   has_one :custom
   has_one :order
   has_one :parcel
