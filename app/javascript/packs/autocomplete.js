d18fbe2a33afdf2b752736e4083f89a97bf41753
Chang-Woo Rhee <x1wins@changwoo.net>
2022-01-11 20:29:47 -0800

    Added setTimeout

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -5,33 +5,44 @@ document.addEventListener("turbolinks:load", function() {
     var input = document.getElementById("q");
     var spinner = document.getElementById("autocomplete-spinner-addon");
     var button = document.getElementById("autocomplete-button-addon");
+    var currentText = "";
     stopAutocompleteSpinner(spinner, button);
     autocomplete({
         input: input,
         fetch: function(text, update) {
+            console.log("text "+text);
             if(text.length < 2){
                 return;
             }
             text = text.toLowerCase();
-            startAutocompleteSpinner(spinner, button);
-            fetch("/shippings/autocomplete?q="+text)
-                .then(handleErrors)
-                .then(response => response.json())
-                .then(json => {
-                    stopAutocompleteSpinner(spinner, button);
-                    update(json);
-                    console.log("ok")
-                    console.log(json)
-                }).catch(error => {
-                    stopAutocompleteSpinner(spinner, button);
-                    console.log(error)
-                });
+            currentText = text;
+            setTimeout(function () {
+                if(currentText == text){
+                    fetchAutocomplete(spinner, button, text, update);
+                }
+            }, 1000)
         },
         onSelect: function(item) {
             input.value = item.label;
         }
     });
 });
+
+function fetchAutocomplete(spinner, button, text, update){
+    startAutocompleteSpinner(spinner, button);
+    fetch("/shippings/autocomplete?q="+text)
+        .then(handleErrors)
+        .then(response => response.json())
+        .then(json => {
+            stopAutocompleteSpinner(spinner, button);
+            update(json);
+            console.log("ok")
+            console.log(json)
+        }).catch(error => {
+        stopAutocompleteSpinner(spinner, button);
+        console.log(error)
+    });
+}
 
 function startAutocompleteSpinner(spinner, button){
     spinner.style.display = "block"
